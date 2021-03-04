31281eab9dbde648c6aa7ed5f6b7ee980cd58af5
Nitin Yadav <nitin.yadav@saavn.com>
2021-03-04 19:13:26 +0530

    Initial Commit

---
+++ User List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  User List
+//
+//  Created by Nitin Yadav on 4/3/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
